605c478267c1a9b1e9a57958e6b26106180574f8
agloks <smoke.day@yahoo.com>
2021-01-02 14:21:21 -0300

    add: route to user login & create & profile

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+# app/controllers/users_controller.rb
+class UsersController < ApplicationController
+  skip_before_action :authorize_request, only: :create
+  
+  #GET /profile
+  def index
+    if @current_user
+      return json_response(@current_user)
+    end
+
+    json_response("not logged")
+  end
+
+  # POST /signup
+  # return authenticated token upon signup
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+  end
+
+  private
+
+  def user_params
+    params.permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation
+    )
+  end
+end
--- backend/spec/rails_helper.rb
+++ backend/spec/rails_helper.rb
@@ -41,7 +41,6 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
-  # config.include RequestSpecHelper, type: :request
   config.include RequestSpecHelper
   config.include ControllerSpecHelper
   
